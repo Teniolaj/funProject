587a7dc9885d6d06a887c4eb542c037b4a7a707b
Teniolaj <87813033+Teniolaj@users.noreply.github.com>
2023-01-03 16:02:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const blackButton = document.querySelector('.black-button')
+const redButton = document.querySelector('.red-button')
+const greenButton = document.querySelector('.green-button')
+
+function changeColorBlack() {
+    document.getElementById('job').style.color = 'black'
+}
+
+function changeColorRed() {
+    document.getElementById('job').style.color = 'red'
+}
+
+function changeColorGreen() {
+    document.getElementById('job').style.color = 'green'
+}
